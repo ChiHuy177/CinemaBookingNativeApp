29900febeb9c86469c810a55e63897fe2b8e2ee7
ChiHuy177 <huynguyenbd54@gmail.com>
2025-11-27 11:52:37 +0700

    init

--- App.tsx
+++ App.tsx
@@ -1,45 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import 'react-native-gesture-handler';
+import AuthProvider from './src/context/AuthContext';
+import SpinnerProvider from './src/context/SpinnerContext';
+import StackNavigator from './src/navigation/StackNavigation';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <SpinnerProvider>
+      <AuthProvider>
+        <StackNavigator />
+      </AuthProvider>
+    </SpinnerProvider>
   );
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
